1daf924adcacf92e7b8259a025aa4c6234ce42a0
Pesto-the-1 <52164990+Pesto-the-1@users.noreply.github.com>
2019-07-18 14:25:30 -0400

    AAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAA

--- Portfolio_javascript.js
+++ Portfolio_javascript.js
@@ -1,7 +1,48 @@
-var zaWarudo=document.getElementById("Za Warudo.mp3");
+var loses = 0;
+var wins = 0;
+			
+var play = function(userChoice) {
+					
+					document.getElementById("player").innerHTML="";
+					document.getElementById("opponent").innerHTML="";
+					document.getElementById("results").innerHTML="";
+				
+					
+					if (userChoice == "rock" || userChoice == "paper" || userChoice == "scissors") {
+						document.getElementById("player").innerHTML='You chose' + ' ' + userChoice + '.';
 
-function audio(){
-    zaWarudo.play();
-}
+					var computerChoice = Math.random();
+					if (computerChoice < 0.34) {
+						computerChoice = "rock";
+					} else if(computerChoice <= 0.67) {
+						computerChoice = "paper";
+					} else {
+						computerChoice = "scissors";
+					}
+				 
+				 	document.getElementById("opponent").innerHTML='| Your opponent chose' + ' ' + computerChoice + '.';
+				 
+					 var compare = function (choice1,choice2) {
+						if (choice1 == choice2) {
+							return "You tied!";
+						} else if (choice1 == "rock"){
+							if (choice2 =="scissors") {
+								return "Rock wins!";
+							} else {
+								return "Sorry, paper wins.";
+							}
+						} else if (choice1 == "paper") {
+							if (choice2 == "rock") {
+								return "Paper wins";
+							} else {
+								return "Sorry, scissors win.";
+							}
+						} else if (choice1 == "scissors") {
+							if (choice2 == "rock") {
+								return "Sorry, rock wins";
+							} else {
+								return "Scissors wins!";
+							}
 
-window.onclick = audio();
+					var winner = compare(userChoice,computerChoice);
+					document.getElementById("results").innerHTML=winner;
